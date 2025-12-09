63d079d1b4b9fa0f1dfb98283855f8758f0f896c
saisravan-kodityala <sirisravan4@gmail.com>
2025-12-09 21:16:10 +0530

    Fix import error by adding view to init

--- store/views/__init__.py
+++ store/views/__init__.py
@@ -5,5 +5,15 @@ from .signup import Signup
 from .cart import Cart
 from .checkout import CheckOut
 from .orders import OrderView
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
+# This function must be here for Python to find it
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        return HttpResponse("Superuser created successfully! <br>Username: <b>admin</b><br>Password: <b>admin123</b>")
+    else:
+        return HttpResponse("Superuser 'admin' already exists.")
 __all__ = ['Index','store','Login','logout','Signup','Cart','CheckOut','OrderView']
